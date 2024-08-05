fbf04c7a71bcb494c20d310dda3e096640580db9
Subham <74097782+shubhamkrp@users.noreply.github.com>
2024-08-05 19:27:10 +0530

    mapping

---
+++ icd9_vector_db.py
@@ -0,0 +1,37 @@
+import numpy as np
+from sentence_transformers import SentenceTransformer
+import faiss
+import re
+
+def read_icd9_cm_terms(file_path):
+    icd9_terms = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            # Use regular expression to split on multiple spaces
+            parts = re.split(r'\s{2,}', line.strip())
+            if len(parts) == 2:
+                code, name = parts
+                icd9_terms.append(f"{code} - {name}")
+    return icd9_terms
+
+# Path to your text file
+file_path = 'icd9_cm_codes.txt'
+icd9_terms = read_icd9_cm_terms(file_path)
+
+# Load pre-trained SBERT model
+# model = SentenceTransformer('all-MiniLM-L6-v2')
+model = SentenceTransformer('dmis-lab/biobert-base-cased-v1.1')
+
+# Generate embeddings for ICD-10-CM terms
+icd9_embeddings = model.encode(icd9_terms)
+
+# Set Up FAISS Vector Database
+# Convert embeddings to a format suitable for FAISS
+icd9_embeddings = np.array(icd9_embeddings).astype('float32')
+
+# Create a FAISS index
+icd9_index = faiss.IndexFlatL2(icd9_embeddings.shape[1])
+icd9_index.add(icd9_embeddings)
+
+faiss.write_index(icd9_index, 'faiss_index_icd9.bin')
+
